a119469084fbe2f43c348fe7ef22d1ad0b9f121b
Yuvraj Bhira <yuvi21554@gmail.com>
2026-05-06 15:28:36 -0400

    updated the startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd /home/site/wwwroot
-composer install --no-dev --optimize-autoloader
+/usr/local/bin/composer install --no-dev --optimize-autoloader
 php artisan migrate --force
 php artisan config:clear
-php artisan cache:clear
-php artisan view:clear
 php artisan storage:link
